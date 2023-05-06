f733acc2e8ddd752ba9164780d7bfc9d53a7626d
j3ff123y <j3ff123y@gmail.com>
2023-05-06 10:22:06 -0500

    future tech

---
+++ Build/Ascended Improvements 3/SQL/GAIA_FutureTech.sql
@@ -0,0 +1,117 @@
+--==========================================================================================================================
+-- BOOSTED FUTURE TECH
+--==========================================================================================================================
+UPDATE Buildings SET EnhancedYieldTech = 'TECH_FUTURE_TECH' WHERE BuildingClass IN (
+	'BUILDINGCLASS_PALACE',
+	'BUILDINGCLASS_IRONWORKS',
+	'BUILDINGCLASS_NATIONAL_TREASURY',
+	'BUILDINGCLASS_GRAND_TEMPLE',
+	'BUILDINGCLASS_HERMITAGE',
+	'BUILDINGCLASS_NATIONAL_COLLEGE'
+);
+
+INSERT INTO Building_TechEnhancedYieldChanges
+		(BuildingType,	YieldType,			Yield)
+SELECT	Type,			'YIELD_FOOD',		4
+FROM Buildings WHERE BuildingClass = 'BUILDINGCLASS_PALACE';
+
+INSERT INTO Building_TechEnhancedYieldChanges
+		(BuildingType,	YieldType,			Yield)
+SELECT	Type,			'YIELD_PRODUCTION',	4
+FROM Buildings WHERE BuildingClass = 'BUILDINGCLASS_IRONWORKS';
+
+INSERT INTO Building_TechEnhancedYieldChanges
+		(BuildingType,	YieldType,			Yield)
+SELECT	Type,			'YIELD_GOLD',		4
+FROM Buildings WHERE BuildingClass = 'BUILDINGCLASS_NATIONAL_TREASURY';
+
+INSERT INTO Building_TechEnhancedYieldChanges
+		(BuildingType,	YieldType,			Yield)
+SELECT	Type,			'YIELD_FAITH',		4
+FROM Buildings WHERE BuildingClass = 'BUILDINGCLASS_GRAND_TEMPLE';
+
+INSERT INTO Building_TechEnhancedYieldChanges
+		(BuildingType,	YieldType,			Yield)
+SELECT	Type,			'YIELD_CULTURE',	4
+FROM Buildings WHERE BuildingClass = 'BUILDINGCLASS_HERMITAGE';
+
+INSERT INTO Building_TechEnhancedYieldChanges
+		(BuildingType,	YieldType,			Yield)
+SELECT	Type,			'YIELD_SCIENCE',	4
+FROM Buildings WHERE BuildingClass = 'BUILDINGCLASS_NATIONAL_COLLEGE';
+
+--==========================================================================================================================
+-- FAKE IMPROVEMENTS for tech tree tooltip
+--==========================================================================================================================
+INSERT INTO Improvements 
+			(Type,						Description, 							Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas)
+SELECT		'IMPROVEMENT_PALACE_DUMMY',	'TXT_KEY_IMPROVEMENT_D_FOR_PALACE', 	Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas 
+FROM Improvements WHERE Type = 'IMPROVEMENT_TRADING_POST'
+AND EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvements 
+			(Type,							Description, 							Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas)
+SELECT		'IMPROVEMENT_IRONWORKS_DUMMY',	'TXT_KEY_IMPROVEMENT_D_FOR_IRONWORKS', 	Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas 
+FROM Improvements WHERE Type = 'IMPROVEMENT_TRADING_POST'
+AND EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvements 
+			(Type,									Description, 									Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas)
+SELECT		'IMPROVEMENT_NATIONAL_TREASURY_DUMMY',	'TXT_KEY_IMPROVEMENT_D_FOR_NATIONAL_TREASURY', 	Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas 
+FROM Improvements WHERE Type = 'IMPROVEMENT_TRADING_POST'
+AND EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvements 
+			(Type,								Description, 								Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas)
+SELECT		'IMPROVEMENT_GRAND_TEMPLE_DUMMY',	'TXT_KEY_IMPROVEMENT_D_FOR_GRAND_TEMPLE', 	Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas 
+FROM Improvements WHERE Type = 'IMPROVEMENT_TRADING_POST'
+AND EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvements 
+			(Type,							Description, 							Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas)
+SELECT		'IMPROVEMENT_HERMITAGE_DUMMY',	'TXT_KEY_IMPROVEMENT_D_FOR_HERMITAGE', 	Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas 
+FROM Improvements WHERE Type = 'IMPROVEMENT_TRADING_POST'
+AND EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvements 
+			(Type,									Description, 									Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas)
+SELECT		'IMPROVEMENT_NATIONAL_COLLEGE_DUMMY',	'TXT_KEY_IMPROVEMENT_D_FOR_NATIONAL_COLLEGE', 	Civilopedia, Help, ArtDefineTag, RequiresFeature, PillageGold, PortraitIndex, IconAtlas 
+FROM Improvements WHERE Type = 'IMPROVEMENT_TRADING_POST'
+AND EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+--==========================================================================================================================
+-- Improvement_TechYieldChanges for tech tree tool tip
+--==========================================================================================================================
+INSERT INTO Improvement_TechYieldChanges
+		(ImprovementType,						TechType,				YieldType,			Yield)
+SELECT	'IMPROVEMENT_PALACE_DUMMY',				'TECH_FUTURE_TECH',		'YIELD_FOOD',		4
+WHERE EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvement_TechYieldChanges
+		(ImprovementType,						TechType,				YieldType,			Yield)
+SELECT	'IMPROVEMENT_IRONWORKS_DUMMY',			'TECH_FUTURE_TECH',		'YIELD_PRODUCTION',	4
+WHERE EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+			
+INSERT INTO Improvement_TechYieldChanges
+		(ImprovementType,						TechType,				YieldType,			Yield)
+SELECT	'IMPROVEMENT_NATIONAL_TREASURY_DUMMY',	'TECH_FUTURE_TECH',		'YIELD_GOLD',		4
+WHERE EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvement_TechYieldChanges
+		(ImprovementType,						TechType,				YieldType,			Yield)
+SELECT	'IMPROVEMENT_GRAND_TEMPLE_DUMMY',		'TECH_FUTURE_TECH',		'YIELD_FAITH',		4
+WHERE EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvement_TechYieldChanges
+		(ImprovementType,						TechType,				YieldType,			Yield)
+SELECT	'IMPROVEMENT_HERMITAGE_DUMMY',			'TECH_FUTURE_TECH',		'YIELD_CULTURE',	4
+WHERE EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+INSERT INTO Improvement_TechYieldChanges
+		(ImprovementType,						TechType,				YieldType,			Yield)
+SELECT	'IMPROVEMENT_NATIONAL_COLLEGE_DUMMY',	'TECH_FUTURE_TECH',		'YIELD_SCIENCE',	4
+WHERE EXISTS (Select * FROM Automates WHERE Type = 'AUTOMATE_MISSIONARY');
+
+--==========================================================================================================================
+
+--==========================================================================================================================
